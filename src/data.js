849b7519d227093b343e4b06e6fa3077663b41ef
Raghavan <66511693+raghavan21@users.noreply.github.com>
2023-06-13 10:38:20 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,38 @@
+export const data = [
+    {
+      id: 1,
+      name: 'Video File',
+      type: 'video',
+      path: "/file-server/robot.mp4"
+    },
+    {
+      id: 2,
+      name: 'Audio File',
+      type: 'audio',
+      path: "/file-server/robot.mp3"
+    },
+    {
+      id: 3,
+      name: 'Document File',
+      type: 'document',
+      path: "/file-server/document.pdf"
+    },
+    {
+      id: 4,
+      name: 'Image File',
+      type: 'image',
+      path: "/file-server/robot.jpg"
+    },
+    {
+        id: 5,
+        name: 'Image File',
+        type: 'image',
+        path: "/file-server/robot2.jpg"
+      },
+      {
+        id: 6,
+        name: 'Image File',
+        type: 'image',
+        path: "/file-server/robot3.jpg"
+      },
+  ]
